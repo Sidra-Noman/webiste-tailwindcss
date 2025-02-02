74f6a0bae166b953ee3a8065b6284acd7413beb2
Sidra Noman <sidranoman09@gmail.com>
2025-02-02 16:42:40 +0500

    Your commit message here

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
     <div className="flex flex-wrap w-full mb-20">
       <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 underline font-serif">
-        "Perfect Fit for Every Move"</h1>
+        Perfect Fit for Every Move</h1>
        
       </div>
       <p className="lg:w-1/2 w-full leading-relaxed text-gray-900 font-serif">Siddnom Jeans offers a range of styles crafted for every lifestyle whether you need rugged durability or sleek urban design.Explore our categories to find the perfect fit that&apos;s as bold and versatile as you are.</p>
@@ -23,7 +23,7 @@ const page = () => {
           <Image  src={"/images/skinny.jpg"} alt="content" height={40} width={250} className=" rounded object-cover object-center mb-6"/>
           <h2 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Category 1</h2>
           <h2 className="text-lg text-gray-900 font-medium mb-4">Skinny Jeans</h2>
-          <p className="leading-relaxed text-base ">Men's skinny jeans offer a sleek, modern fit that hugs the legs for a sharp, streamlined look. Perfect for a night out or casual wear, these jeans bring style and comfort together effortlessly.</p>
+          <p className="leading-relaxed text-base ">Men&apos;s skinny jeans offer a sleek, modern fit that hugs the legs for a sharp, streamlined look. Perfect for a night out or casual wear, these jeans bring style and comfort together effortlessly.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
